d079682c17fa4c85dba9a3c896da944696d619bb
ketulkumar <k2lsuthar@gmail.com>
2020-07-14 22:18:54 -0400

    Create class based component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
 function App() {
   return (
     <div className="App">
       <h1>First ,React App</h1>
     </div>
   );
-}
+}*/
 
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h1>First ,React App</h1>
+      </div>
+    );
+  }
+}
 export default App;
