c7d5554ee4778de61534b471ba89d676c53238b0
ketulkumar <k2lsuthar@gmail.com>
2020-07-14 22:50:12 -0400

    Pass value to person component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>First ,React App</h1>
         <p> THis is really working.</p>
-        <Person/>
-        <Person/>
-        <Person/>
+        <Person name="Ketul" age="30"/>
+        <Person name="Nil" age="27"> My Hobbies : Cricket</Person>
+        <Person name="Jiger" age="31"/>
       </div>
     );
     //return React.createElement('div',{ className:'App'}, React.createElement('h1',null,'Element created using CreateElement'))
