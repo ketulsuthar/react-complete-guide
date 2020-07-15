cefb6d3bda3da1ceb2cf3d67e213a495a8a52e2e
ketulkumar <k2lsuthar@gmail.com>
2020-07-15 00:27:53 -0400

    Added state property

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Ketul',age : 30},
+      {name: 'Nil',age : 27},
+      {name: 'Jiger',age : 31}
+    ]
+
+  }
+
   render(){
     return(
       <div className="App">
         <h1>First ,React App</h1>
         <p> THis is really working.</p>
-        <Person name="Ketul" age="30"/>
-        <Person name="Nil" age="27"> My Hobbies : Cricket</Person>
-        <Person name="Jiger" age="31"/>
+        <button>Switch Name</button>
+        <Person name= {this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies : Cricket</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div',{ className:'App'}, React.createElement('h1',null,'Element created using CreateElement'))
